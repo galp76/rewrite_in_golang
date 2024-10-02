b5d2950e1b695cba50ed5068fbfe769b972901b1
German Lobo <germanlobo76@gmail.com>
2024-10-02 13:12:45 -0400

    Se continúa con la implementación de main.go

---
+++ autenticacion.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"bufio"
+	"strings"
+)
+
+func autenticacion() (bool, string) {
+	var usuario string;
+	for {
+		fmt.Printf("\nIndique su usuario: ");
+		var _, err = fmt.Scanln(&usuario);
+		if err != nil {
+			fmt.Println("Error procesando la información.");
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	time.Sleep(1 * time.Second);
+	var clave string;
+	for {
+		fmt.Printf("\nIndique su clave: ");
+		var _, err = fmt.Scanln(&clave);
+		if err != nil {
+			fmt.Println("Error procesando la información.");
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	var verified bool;
+	readFile, err := os.Open("users.txt");
+    if err != nil {
+        fmt.Println(err);
+    }
+    fileScanner := bufio.NewScanner(readFile);
+    fileScanner.Split(bufio.ScanLines);
+    for fileScanner.Scan() {
+        var line = fileScanner.Text();
+		var parts []string = strings.Split(line, ";");
+		if parts[0] == usuario && parts[1] == clave {
+			verified = true;
+			break;
+		}
+    }
+    readFile.Close();
+
+	return verified, usuario;
+}
