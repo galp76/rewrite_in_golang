b2d67b4d27973e4784e138cd8d6ed512d556b7e5
German Lobo <germanlobo76@gmail.com>
2024-10-15 13:18:13 -0400

    Se corrigieron errores de lógica en tarea.go

--- tarea.go
+++ tarea.go
@@ -96,7 +96,6 @@ func tarea(sesion string, respaldo bool, usuario string) int {
 	};
 
 	readFile, err := os.Open(directorio);
-    defer readFile.Close();
     if err != nil {
 		prompt = fmt.Sprintf("\nHubo un error leyendo el archivo %s: %s.", directorio, err);
 		fmt.Println(prompt);
@@ -112,6 +111,7 @@ func tarea(sesion string, respaldo bool, usuario string) int {
 		sleep();
 		os.Exit(1);
     }
+    defer readFile.Close();
     fileScanner := bufio.NewScanner(readFile);
     fileScanner.Split(bufio.ScanLines);
 	i := 0;
@@ -132,7 +132,7 @@ func tarea(sesion string, respaldo bool, usuario string) int {
 		i++;
     }
 
-	for _, linea := range ejercicios {
+	for i, linea := range ejercicios {
 		partes := strings.Split(linea, " ");
 		if partes[2] == "Resuelto" {
 			continue;
@@ -144,18 +144,10 @@ func tarea(sesion string, respaldo bool, usuario string) int {
 			archivoAgregar(sesion, prompt);
 		}
 
+		var control int;
 		switch partes[0] {
 		case "1":
-			control := mainSuma(sesion, respaldo, true, partes[1]);
-			if control == 0 {
-				prompt = "\nEl ejercicio fue hecho adecuadamente.";
-			} else {
-				prompt = "\nEl usuario decidio no terminar el ejercicio.";
-			}
-			fmt.Println(prompt);
-			if respaldo {
-				archivoAgregar(sesion, prompt);
-			}
+			control = mainSuma(sesion, respaldo, true, partes[1]);
 		default:
 			prompt = "\nIntroduce \"s\" para salir del sistema, o presiona ENTER para continuar con el siguiente ejercicio.\n\nOpción:";
 			fmt.Println(prompt);
@@ -164,7 +156,69 @@ func tarea(sesion string, respaldo bool, usuario string) int {
 			}
 			_ = obtenerEntradaUsuario(sesion, respaldo);
 		}
+		if control == 0 {
+			marcarComoResuelto(i, directorio, sesion, respaldo);
+			sleep();
+			prompt = "\nEl ejercicio fue marcado como Resuelto.";
+		} else {
+			prompt = "\nEl ejercicio sigue marcado Como Pendiente.";
+		}
+		fmt.Println(prompt);
+		if respaldo {
+			archivoAgregar(sesion, prompt);
+		}
+		sleep();
+		prompt = "\nIntroduce \"s\" para salir del sistema, o presiona ENTER para continuar con el siguiente ejercicio.\n\nOpción:";
+		fmt.Println(prompt);
+		if respaldo {
+			archivoAgregar(sesion, prompt);
+		}
+		_ = obtenerEntradaUsuario(sesion, respaldo);
 	}
 
 	return 0;
+}
+
+func marcarComoResuelto(numero int, archivo string, sesion string, respaldo bool) {
+	archivoTemporal, err := os.Create("archivoTemporal.txt");
+	if err != nil {
+		fmt.Println("\nEl ejercicio no pudo ser marcado como resuelto.");
+		sleep();
+		return;
+	}
+	archivoTemporal.Close();
+
+	readFile2, err := os.Open(archivo);
+    if err != nil {
+		prompt := fmt.Sprintf("\nHubo un error leyendo el archivo %s: %s.", archivo, err);
+		fmt.Println(prompt);
+		if respaldo {
+			archivoAgregar(sesion, prompt);
+		}
+		sleep();
+		prompt = "\nEl ejercicio no pudo ser marcado como resuelto.";
+		fmt.Println(prompt);
+		if respaldo {
+			archivoAgregar(sesion, prompt);
+		}
+		sleep();
+		return;
+    }
+    defer readFile2.Close();
+    fileScanner2 := bufio.NewScanner(readFile2);
+    fileScanner2.Split(bufio.ScanLines);
+	i := 0;
+    for fileScanner2.Scan() {
+        var ejercicio = fileScanner2.Text();
+		if i == numero {
+			partes := strings.Split(ejercicio, " ");
+			lineaTemporal := fmt.Sprintf("%s %s %s", partes[0], partes[1], "Resuelto");
+			archivoAgregar("archivoTemporal.txt", lineaTemporal);
+		} else {
+			archivoAgregar("archivoTemporal.txt", ejercicio);
+		}
+		i++;
+	}
+
+	os.Rename("archivoTemporal.txt", archivo);
 }
