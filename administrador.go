e4616976199c95cf17410a63a748738d62da64a8
German Lobo <germanlobo76@gmail.com>
2024-10-19 20:10:28 -0400

    Se sigue trabajando en administrador.go

--- administrador.go
+++ administrador.go
@@ -36,12 +36,12 @@ func crearUsuario(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(html));
 }
 
-// retorna true si el usuario es nuevo
-func validarNuevoUsuario(usuario string) bool {
+// retorna true si el usuario es nuevo, toma en consideracion el usuario y el grupo
+func validarNuevoUsuario(usuario string, grupo string) bool {
 	usuarios, _ := fileToSlice("users.txt");
 	for _, linea := range usuarios {
 		partes := strings.Split(linea, ";");
-		if partes[0] == usuario {
+		if partes[0] == usuario && partes[2] == grupo {
 			return false;
 		}
 	}
@@ -52,20 +52,39 @@ func validarNuevoUsuario(usuario string) bool {
 func procesarNuevoUsuario(w http.ResponseWriter, r *http.Request) {
 	linea := r.URL.Path[len("/procesarNuevoUsuario/"):];
 	datos := strings.Split(linea, "/");
-	if validarNuevoUsuario(datos[0]) {
+	if validarNuevoUsuario(datos[0], datos[2]) {
 		nuevoUsuario := fmt.Sprintf("%s;%s;%s", datos[0], datos[1], datos[2]);
 		archivoAgregar("users.txt", nuevoUsuario);
-//		html, _ := cargarHtml("html/usuarios/crearUsuarioExito.html");
-//		fmt.Fprintln(w, string(html));
-		http.Redirect(w, r, "html/usuarios/crearUsuarioExito.html", 303);
+		http.Redirect(w, r, "/usuarioCreado", 303);
+	} else {
+		http.Redirect(w, r, "/usuarioYaExiste", 303);
 	}
+}
+
+func usuarioCreado(w http.ResponseWriter, r *http.Request) {
+	html, _ := cargarHtml("html/usuarios/usuarioCreado.html");
+	fmt.Fprintln(w, string(html));
+}
+
+func usuarioYaExiste(w http.ResponseWriter, r *http.Request) {
+	html, _ := cargarHtml("html/usuarios/usuarioYaExiste.html");
+	fmt.Fprintln(w, string(html));
+}
+
+func borrarUsuario(w http.ResponseWriter, r *http.Request) {
+	html, _ := cargarHtml("html/usuarios/borrarUsuario.html");
+	fmt.Fprintln(w, string(html));
 }
 
 func mainAdministrador() {
 //	http.HandleFunc("/view/", viewHandler);
 	http.HandleFunc("/", index);		// de las mias
+	http.HandleFunc("/menuPrincipal", index);		// de las mias
 	http.HandleFunc("/crearUsuario", crearUsuario);		// de las mias
 	http.HandleFunc("/procesarNuevoUsuario/", procesarNuevoUsuario);		// de las mias
+	http.HandleFunc("/usuarioCreado", usuarioCreado);		// de las mias
+	http.HandleFunc("/usuarioYaExiste", usuarioYaExiste);		// de las mias
+	http.HandleFunc("/borrarUsuario", borrarUsuario);		// de las mias
 	fmt.Println("Iniciando servidor...");
 	log.Fatal(http.ListenAndServe(":8080", nil));
 }
